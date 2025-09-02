2eb5273364ec9ce4030df3fce435066a9eb9fdf1
shirin17h <shirin17h@gmail.com>
2025-09-02 12:37:17 +0300

    refactor: remove unused admin and consumer models, rename driver to deliverer, and update user role default to 'customer'

--- backend/src/models/deliverer.model.ts
+++ backend/src/models/deliverer.model.ts
@@ -1,18 +1,18 @@
 import mongoose from 'mongoose';
 import User, { IUser } from './user.model';
 
-export interface IDriver extends IUser {
+export interface IDeliverer extends IUser {
   availability?: Date[];
   assignedDeliveries?: string[];
   rating?: number;
   vehicle?: mongoose.Types.ObjectId;
 }
 
-const Driver = User.discriminator<IDriver>('Driver', new mongoose.Schema({
+const Deliverer = User.discriminator<IDeliverer>('Deliverer', new mongoose.Schema({
   availability: [{ type: Date }],
   assignedDeliveries: [{ type: String }],
   rating: { type: Number, default: 0 },
   vehicle: { type: mongoose.Schema.Types.ObjectId, ref: 'Vehicle' },
 }));
 
-export default Driver;
+export default Deliverer;
