34e5f09a95073ca43d6943f06647344a71872eb5
TheXJoKeR71 <mhmd_dragon1@hotmail.com>
2025-09-09 14:43:37 +0300

    added basic items system, needs to be tested

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import authRoutes from './auth.route';
 import orderRoutes from './orders.route';
+import itemRoutes from './items.route'
 import aggregationsRoutes from './aggregations.route';
 import containersRoutes from './containers.route';
 import shipmentsRoutes from './shipments.route';
@@ -8,6 +9,7 @@ import jobApplicationRouter from "./jobApplication.route";
 
 const router = Router();
 router.use('/auth', authRoutes);
+router.use('/items', itemRoutes);
 router.use('/orders', orderRoutes);
 router.use('/aggregations', aggregationsRoutes);
 router.use('/containers', containersRoutes);
